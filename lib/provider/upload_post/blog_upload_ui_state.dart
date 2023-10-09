d666e79a8bfd568ef9870818c828f3fd258ceeac
MohanPoudel2 <mohanpdl3432@gmail.com>
2023-10-09 12:25:12 +0630

    blog upload state+prover done

---
+++ lib/provider/upload_post/blog_upload_ui_state.dart
@@ -0,0 +1,23 @@
+import 'package:blog_rest_api_provider/data/model/blog_upload_response.dart';
+
+abstract class BlogUploadUiState{}
+
+class BlogUploadLoading extends BlogUploadUiState{
+  final int progress;
+
+  BlogUploadLoading(this.progress);
+
+}
+class BlogUploadSuccess extends BlogUploadUiState{
+  final BlogUploadResponse blogUploadResponse;
+
+  BlogUploadSuccess(this.blogUploadResponse);
+
+
+}
+class BlogUploadFailed extends BlogUploadUiState{
+  final String errorMessage;
+
+  BlogUploadFailed(this.errorMessage);
+
+}
